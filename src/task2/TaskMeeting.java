06e8cca8ffc521a88bb10f1875bd847140abb753
NikWork <pnips@mail.ru>
2019-07-04 17:30:03 +0300

    new class Meeting

--- src/task2/TaskMeeting.java
+++ src/task2/TaskMeeting.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class TaskMeeting {
@@ -16,6 +18,7 @@ public class TaskMeeting {
 
 	//	long unixTime = System.currentTimeMillis() / 1000L;
 	//	System.out.println(unixTime);
+		List<Meeting> ListMeeting=new ArrayList<>();
 
 		int NRequest = Integer.parseInt(br.readLine());
 		for (int i = 0; i < NRequest; ++i)
@@ -32,12 +35,14 @@ public class TaskMeeting {
 				int duration = Integer.parseInt(tk.nextToken());
 				int peopleN = Integer.parseInt(tk.nextToken());
 				String people[] = new String[peopleN];
-
+				
 				for (int j = 0; j < peopleN; ++j)
 				{
 					people[j] = tk.nextToken();
 				}
+				Meeting met= new Meeting(day,hour,minute,duration,peopleN);
 
+				ListMeeting.add(met); //список всех встреч			
 				int start = (hour * 60) + minute;
 				int end = start + duration;
 
@@ -50,14 +55,23 @@ public class TaskMeeting {
 
 //класс с данными по встречам
 class Meeting{
+	private int dayYear;
 	private int hour;
 	private int minute;
 	private int duration;
+	private int peopleN;
+	private String[] people;
 	
-	public Meeting(int h, int m, int d) {
+	public Meeting(int day, int h, int m, int d, int pn) {
+		dayYear=day;
 		hour=h;
 		minute=m;
 		duration=d;
+		peopleN=pn;
+//		people=name;
+	}
+	public int getDay() {
+		return dayYear;
 	}
 	public int getHour() {
 		return hour;
@@ -68,5 +82,11 @@ class Meeting{
 	public int getDuration() {
 		return duration;
 	}
+	public int getPeopleN() {
+		return peopleN;
+	}
+/*	public String[] getListPeople() {
+		return people;
+	}*/
 }
 
